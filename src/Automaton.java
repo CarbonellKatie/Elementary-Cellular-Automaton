55fa8f77a7636049f78419894e07aeee12b8b52c
CarbonellKatie <72585228+CarbonellKatie@users.noreply.github.com>
2021-03-04 20:44:02 -0600

    all working - no javadoc

--- src/Automaton.java
+++ src/Automaton.java
@@ -1,5 +1,10 @@
+import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.StringJoiner;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Automaton {
 public char trueSymbol;
 public char falseSymbol;
@@ -7,15 +12,27 @@ private Rule rule;
 private int steps = 0;
 
 private ArrayList<Generation> generations = new ArrayList<Generation>();
+	
 	public Automaton(int ruleNum, Generation initial) {
-		trueSymbol = '0';
-		falseSymbol = '1';
+		trueSymbol = '1';
+		falseSymbol = '0';
 		rule = new Rule(ruleNum);
 		generations.add(initial);
 	}
 
-	public Automaton(String filename){
-		rule = new Rule(6);
+	public Automaton(String filename) throws FileNotFoundException, IOException {
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		String currInfoString = br.readLine();
+		while(currInfoString != null) {
+		rule = new Rule(Integer.parseInt(currInfoString));
+		currInfoString = br.readLine();
+		falseSymbol = currInfoString.charAt(0);
+		trueSymbol = currInfoString.charAt(2);
+		currInfoString = br.readLine();
+		Generation gen = new Generation(currInfoString, trueSymbol);
+		generations.add(gen);
+		currInfoString = br.readLine();
+	}
 	}
 
 public int getRuleNum() {
@@ -47,16 +64,15 @@ public Generation getGeneration(int stepNum) {
 }
 
 public int getTotalSteps() {
-	return generations.size();
+	return generations.size() - 1;
 }
 
 public String toString() {
 	String str = "";
-	StringJoiner strJ = new StringJoiner("");
+	StringJoiner strJ = new StringJoiner(System.lineSeparator());
 	for(int i = 0; i < generations.size(); i++) {
-		str = (generations.get(i)).getStates(trueSymbol, falseSymbol);
+		str = (generations.get(i)).getStates(falseSymbol, trueSymbol);			//flipped these
 		strJ.add(str);
-		strJ.add(System.lineSeparator());
 	}
 	return strJ.toString();
 	
